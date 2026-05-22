15208ce4efe52ae14980d59914cc51bd7c79e5ad
Tommy <2071882604@qq.com>
2026-05-22 15:29:15 +0800

    新增了实验1、实验2和实验3

---
+++ Ex1/main.m
@@ -0,0 +1,127 @@
+% 实验一 图像处理的基本操作
+clc;
+clear;
+close all;
+% 实验步骤
+% 1.图像的读写
+% (A) 图像读
+I=imread('boat.bmp');
+% (B) 图像写
+whos I
+imwrite(I, 'boat1.bmp');
+
+%2.图像显示
+% (A)灰度图像的显示
+
+I=imread('boat.bmp');
+imshow(I)
+
+%(B)RGB图像的显示
+RGB=imread('flower.tif');
+imshow(RGB)
+figure
+imshow(RGB(:,:,3))  % 显示第3个颜色分量
+
+%3、图像的基本运算
+
+%(A)图像的缩放
+A=imread('lena.tif');
+B1=imresize(A,0.5);
+imshow(A);title('原图');
+figure,imshow(B1);title('比例缩小图');
+
+
+
+% (B)图像的旋转
+clear all;
+clc;
+I=imread('lena.tif');
+J=imrotate(I,90);         %图像进行逆时针旋转90度
+K=imrotate(I,180);
+subplot(1,3,1);imshow(I);title('原图');
+subplot(1,3,2);imshow(J);title('旋转90度图');
+subplot(1,3,3);imshow(K);title('旋转180度图');
+
+%4、彩色图像分通道显示及常用的彩色空间的转换
+%(A)彩色图像的分通道显示
+%（1）彩色图像的分通道显示
+clear all;
+clc;
+RGB=imread('peppers.bmp');
+R=RGB(:,:,1);                   %R通道
+G=RGB(:,:,2);                   %G通道
+B=RGB(:,:,3);                   %B通道
+subplot(2,2,1);imshow(RGB);title('原始图像');
+subplot(2,2,2);imshow(R);title('R分量');
+subplot(2,2,3);imshow(G);title('G分量');
+subplot(2,2,4);imshow(B);title('B分量');
+
+%(B)常用的彩色空间的转换
+clear all;
+clc;
+RGB=imread('peppers.bmp');
+hsi_image = rgb2hsi(RGB);       %RGB空间转换为HSV空间
+YCBCR = rgb2ycbcr(RGB);         %RGB空间转换为YCBCR空间
+subplot(1,3,1);imshow(hsi_image(:,:,1));title('h分量图');
+subplot(1,3,2);imshow(hsi_image(:,:,2));title('s分量图');
+subplot(1,3,3);imshow(hsi_image(:,:,3));title('i分量图');
+figure,
+subplot(1,3,1);imshow(YCBCR(:,:,1));title('Y分量图');
+subplot(1,3,2);imshow(YCBCR(:,:,2));title('cb分量图');
+subplot(1,3,3);imshow(YCBCR(:,:,3));title('cr分量图');
+
+%5、图像的直方图显示与图像的灰度直方图均衡化
+%(1)图像的直方图显示
+%A．在matlab环境中，程序首先读取图像，然后调用直方图函数，设置相关参数，再输出处理后的图像。
+
+I=imread('cameraman1.tif');              %读取图像
+subplot(1,2,1),imshow(I)                %输出图像
+title('原始图像')                           %在原始图像中加标题
+subplot(1,2,2),imhist(I)                  %输出原图直方图
+title('原始图像直方图')                     %在原图直方图上加标题
+
+%B．读入图像，在一个窗口中显示灰度级n=64，128和256的图像直方图。
+
+I=imread('rice1.tif');
+subplot(2,2,1);imshow(I);title('原始图像')
+subplot(2,2,2);imhist(I,64);title('n=64的直方图')
+subplot(2,2,3);imhist(I,128);title('n=128的直方图')
+subplot(2,2,4);imhist(I,256);title('n=256的直方图')
+
+
+%C.读入图像'pollen1.tif '和图像'pollen2.tif '，观察其图像和对应的灰度直方图显示。
+
+clear all;
+clc;
+I=imread('pollen1.tif');
+J= imread('pollen2.tif');
+subplot(2,2,1);imshow(I);
+subplot(2,2,2);imhist(I,256);
+subplot(2,2,3);imshow (J);
+subplot(2,2,4);imhist(J,256);
+
+%D. 读入图像'boat.bmp ',将其旋转180度，观察原图和旋转180度后图像的灰度直方图。
+
+clear all;
+clc;
+I=imread('boat.bmp');
+J= imrotate(I,180);
+subplot(2,2,1);imshow(I);
+subplot(2,2,2);imhist(I,256);
+subplot(2,2,3);imshow (J);
+subplot(2,2,4);imhist(J,256);
+
+%(2) 图像的灰度直方图均衡化
+% 在matlab环境中，程序首先读取图像，然后调用灰度直方图均衡化函数，设置相关参数，再输出处理后的图像;观察原图像和直方图均衡化后图像的显示效果，并显示原图像和直方图均衡化后图像的直方图。
+clear all;
+clc;
+I=imread('pollen1.tif');              %读取图像
+subplot(2,2,1),imshow(I)            %输出图像
+title('原始图像')                   %在原始图像中加标题
+subplot(2,2,3),imhist(I,256)                  %输出原图直方图
+title('原始图像直方图')                     %在原图直方图上加标题
+J=histeq(I,256);                    %直方图均衡化，灰度级为256
+subplot(2,2,2),imshow(J)                %输出均衡化后图像
+title('均衡化后图像')                  %在均衡化后图像中加标题
+subplot(2,2,4),imhist(J,256)             %输出均衡化后直方图
+title('均衡化后图像直方图')             %在均衡化后直方图上加标题
---
+++ Ex3/main.m
@@ -0,0 +1,138 @@
+% 实验三    图像二维离散傅里叶变换与性质
+clc;
+clear;
+close all;
+
+% 1. 显示图像的离散傅里叶变换的幅度谱和相位谱
+% (1) 显示图像的离散傅里叶变换的幅度谱
+I = imread('cameraman1.tif');   % 读取图像
+J = fftshift(fft2(I));          % 求图像的傅里叶变换谱并进行频谱中心化
+figure;
+imshow(I);
+title('原始图像');
+figure;
+imshow(log(1 + abs(J)), []);
+title('原始图像的幅度谱');      % 采用对数方法显示原图的幅度谱
+
+% (2) 显示图像的离散傅里叶变换的幅度谱和相位谱
+I = imread('lena.tif'); % 读取图像
+J = fftshift(fft2(I));  % 求图像的傅里叶变换谱并进行频谱中心化
+figure;
+subplot(1, 2, 1);
+imshow(angle(J));
+title('原图像相位谱');    % 显示原图的相位谱
+subplot(1, 2, 2);
+imshow(log(1 + abs(J)), []);
+title('原图像的幅度谱');   % 对数方法显示原图的幅度谱
+
+
+% (3) 显示高斯低通滤波输出图的幅度谱
+I = imread('cameraman1.tif');   % 读取图像
+J = fftshift(fft2(I));          % 求图像的傅里叶变换谱并进行频谱中心化
+h = fspecial('gaussian', 21, 2);
+K = imfilter(I, h);             % 求图像的高斯低通滤波
+L = fftshift(fft2(K));
+figure;
+subplot(1, 2, 1);
+imshow(log(1 + abs(J)), []);
+title('原始图像的幅度谱');      % 对数方法显示原图幅度谱
+subplot(1, 2, 2);
+imshow(log(1 + abs(L)), []);
+title('高斯低通滤波输出图的幅度谱'); % 采用对数方法显示高斯低通滤波输出图的幅度谱
+
+% 2. 二维离散傅里叶逆变换
+I = imread('lena.tif'); % 读取图像
+J = fft2(I);            % 求图像的傅里叶变换谱
+K = ifft2(J);           % 采用二维离散傅里叶逆变换重构图像
+figure;
+subplot(1, 2, 1);
+imshow(I);
+title('原始图像');
+subplot(1, 2, 2);
+imshow(real(K), []);
+title('傅里叶变换谱的重构图');    % 傅里叶变换谱的重构图
+
+
+% 3. 傅里叶变换谱实部的二维离散傅里叶逆变换
+I = imread('lena.tif'); % 读取图像
+J = real(fft2(I));      % 求图像傅里叶变换谱的实部
+K = ifft2(J);
+figure;
+subplot(1, 2, 1);
+imshow(I);
+title('原始图像');
+subplot(1, 2, 2);
+imshow(real(K), []);
+title('傅里叶变换谱实部的重构图');  % 显示傅里叶变换谱实部的重构图	
+
+% 4. 傅里叶变换相位信息的二维离散傅里叶逆变换
+I = imread('lena.tif'); % 读取图像
+J = fft2(I)./abs(fft2(I));   
+K = ifft2(J);
+figure;
+subplot(1, 2, 1);
+imshow(I);
+title('原始图像');
+subplot(1, 2, 2);
+imshow(real(K), []);
+title('傅里叶变换相位信息的重构图'); % 显示傅里叶变换相位信息的重构图	
+
+% 5. 图像二维离散傅里叶变换的性质
+% (1) 二维离散傅里叶变换的可分离性
+I = imread('lena.tif'); % 读取图像
+[M, N] = size(I);
+J = zeros(M, N);
+K = zeros(M, N);
+for i = 1 : M
+    J(i, :) = fft(I(i, :)); % 先执行行方向一维 FFT
+end
+for j = 1 : N
+    K(:, j) = fft(J(:, j)); % 接着执行列方向的一维 FFT
+end
+H = ifft2(K);   % 对图像的傅里叶变换谱执行二维离散傅里叶逆变换
+figure;
+subplot(1, 2, 1);
+imshow(log(1+abs(fftshift(K))), []);
+title('原始图像的幅度谱');
+subplot(1, 2, 2);
+imshow(real(H), []);
+title('傅里叶变换谱的重构图');
+
+% (2) 二维离散傅里叶变换的旋转性质
+I = imread('boat.bmp');
+J = imrotate(I, 90);
+K = fftshift(fft2(I));
+L = fftshift(fft2(J));
+figure;
+subplot(1, 2, 1);
+imshow(log(1+abs(K)), []);
+title('原始图像的幅度谱');
+subplot(1, 2, 2);
+imshow(log(1+abs(L)), []);
+title('图像旋转90度后的幅度谱');
+
+% (3) 二维离散傅里叶变换的共轭对称性质
+I=imread('lena.tif'); %读取图像
+J=fft2(I);   
+K=conj(J);  %对傅里叶变换谱求共轭
+H=ifft2(K); 
+imshow(real(H),[]);title('傅里叶变换谱求共轭后的重构图')
+
+% (4) 二维离散傅里叶变换的平移性质
+I=imread('cameraman1.tif');
+J=fftshift(I);  %对图像I进行循环移位
+K=fftshift(fft2(I));   %求原图像的傅里叶变换谱并进行频谱中心化
+L=fftshift(fft2(J));   %求平移后图像的傅里叶变换谱并进行频谱中心化
+subplot(1,2,1),imshow(log(1+abs(K)),[]);title('原始图像的幅度谱')  
+subplot(1,2,2),imshow(log(1+abs(L)),[]);title('图像平移后的幅度谱')  
+
+% (5) 二维离散傅里叶变换的卷积定理和相关定理
+% 基于相关运算，通过模板匹配检测文本图像中的字符a,
+textimage = imread('text1.png');
+a = textimage(32:45,88:98);   %字符a的模板图像 
+imshow(textimage);
+figure, imshow(a);
+C = real(ifft2(fft2(textimage) .* fft2(rot90(a,2),256,256)));    
+figure, imshow(C,[]) 
+thresh = 60;     % 设置相关峰的检测阈值
+figure, imshow(C > thresh);   % 检测大于阈值的相关峰，则对应的位置为字符a
