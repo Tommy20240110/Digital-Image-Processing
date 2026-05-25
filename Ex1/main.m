61c12b98ef55536985c9306e3c1df5a9dad0a2e4
Tommy <2071882604@qq.com>
2026-05-25 17:45:36 +0800

    规范化了实验1和实验2

--- Ex1/main.m
+++ Ex1/main.m
@@ -1,127 +1,166 @@
-% 实验一 图像处理的基本操作
+% 实验一 图像处理的基本操作和图像直方图均衡化
 clc;
 clear;
 close all;
-% 实验步骤
-% 1.图像的读写
-% (A) 图像读
-I=imread('boat.bmp');
-% (B) 图像写
-whos I
-imwrite(I, 'boat1.bmp');
 
-%2.图像显示
-% (A)灰度图像的显示
+%% 1. 图像读写
+% (A) 图像读出
+I = imread('Ex1/boat.bmp');
 
-I=imread('boat.bmp');
-imshow(I)
+% (B) 图像写入
+whos I;
+imwrite(I, 'Ex1/boat1.bmp');
 
-%(B)RGB图像的显示
-RGB=imread('flower.tif');
-imshow(RGB)
-figure
-imshow(RGB(:,:,3))  % 显示第3个颜色分量
+%% 2. 图像显示
+% (A) 灰度图像的显示
+figure;
+imshow(I);
 
-%3、图像的基本运算
+% (B) RGB 图像的显示
+RGB = imread('Ex1/flower.tif');
+figure;
+imshow(RGB);
+figure;
+imshow(RGB(:,:,3));  % 显示第 3 个颜色分量
 
-%(A)图像的缩放
-A=imread('lena.tif');
-B1=imresize(A,0.5);
-imshow(A);title('原图');
-figure,imshow(B1);title('比例缩小图');
+%% 3. 图像的基本运算
+% (A) 图像的缩放
+A = imread('Ex1/lena.tif');
+B = imresize(A, 0.5);
+figure;
+imshow(A);
+title('原图');
+figure;
+imshow(B);
+title('比例缩小图');
 
+% (B) 图像的旋转
+I = imread('Ex1/lena.tif');
+J = imrotate(I, 90);    % 图像逆时针旋转 90 度
+K = imrotate(I, 180);
+figure;
+subplot(1,3,1);
+imshow(I);
+title('原图');
+subplot(1,3,2);
+imshow(J);
+title('旋转 90 度图');
+subplot(1,3,3);
+imshow(K);
+title('旋转 180 度图');
 
+%% 4. 彩色图像分通道显示及常用彩色空间转换
+% (A) 彩色图像的分通道显示
+RGB = imread('Ex1/peppers.bmp');
+R = RGB(:,:,1);  % R 通道
+G = RGB(:,:,2);  % G 通道
+B = RGB(:,:,3);  % B 通道
+figure;
+subplot(2,2,1);
+imshow(RGB);
+title('原始图像');
+subplot(2,2,2);
+imshow(R);
+title('R 分量');
+subplot(2,2,3);
+imshow(G);
+title('G 分量');
+subplot(2,2,4);
+imshow(B);
+title('B 分量');
 
-% (B)图像的旋转
-clear all;
-clc;
-I=imread('lena.tif');
-J=imrotate(I,90);         %图像进行逆时针旋转90度
-K=imrotate(I,180);
-subplot(1,3,1);imshow(I);title('原图');
-subplot(1,3,2);imshow(J);title('旋转90度图');
-subplot(1,3,3);imshow(K);title('旋转180度图');
+% (B) 常用彩色空间转换
+RGB = imread('Ex1/peppers.bmp');
+HSI = rgb2hsi(RGB);    % RGB 空间转换为 HSI 空间
+YCBCR = rgb2ycbcr(RGB);      % RGB 空间转换为 YCbCr 空间
+figure;
+subplot(1,3,1);
+imshow(HSI(:,:,1));
+title('H 分量图');
+subplot(1,3,2);
+imshow(HSI(:,:,2));
+title('S 分量图');
+subplot(1,3,3);
+imshow(HSI(:,:,3));
+title('I 分量图');
+figure;
+subplot(1,3,1);
+imshow(YCBCR(:,:,1));
+title('Y 分量图');
+subplot(1,3,2);
+imshow(YCBCR(:,:,2));
+title('Cb 分量图');
+subplot(1,3,3);
+imshow(YCBCR(:,:,3));
+title('Cr 分量图');
 
-%4、彩色图像分通道显示及常用的彩色空间的转换
-%(A)彩色图像的分通道显示
-%（1）彩色图像的分通道显示
-clear all;
-clc;
-RGB=imread('peppers.bmp');
-R=RGB(:,:,1);                   %R通道
-G=RGB(:,:,2);                   %G通道
-B=RGB(:,:,3);                   %B通道
-subplot(2,2,1);imshow(RGB);title('原始图像');
-subplot(2,2,2);imshow(R);title('R分量');
-subplot(2,2,3);imshow(G);title('G分量');
-subplot(2,2,4);imshow(B);title('B分量');
+%% 5. 图像的直方图显示与灰度直方图均衡化
+% (A) 图像的直方图显示
+% 1. 显示原始图像及其直方图
+I = imread('Ex1/cameraman1.tif');
+figure;
+subplot(1,2,1);
+imshow(I);
+title('原始图像');
+subplot(1,2,2);
+imhist(I);
+title('原始图像直方图');
 
-%(B)常用的彩色空间的转换
-clear all;
-clc;
-RGB=imread('peppers.bmp');
-hsi_image = rgb2hsi(RGB);       %RGB空间转换为HSV空间
-YCBCR = rgb2ycbcr(RGB);         %RGB空间转换为YCBCR空间
-subplot(1,3,1);imshow(hsi_image(:,:,1));title('h分量图');
-subplot(1,3,2);imshow(hsi_image(:,:,2));title('s分量图');
-subplot(1,3,3);imshow(hsi_image(:,:,3));title('i分量图');
-figure,
-subplot(1,3,1);imshow(YCBCR(:,:,1));title('Y分量图');
-subplot(1,3,2);imshow(YCBCR(:,:,2));title('cb分量图');
-subplot(1,3,3);imshow(YCBCR(:,:,3));title('cr分量图');
+% 2. 不同灰度级的直方图显示
+I = imread('Ex1/rice1.tif');
+figure;
+subplot(2,2,1);
+imshow(I);
+title('原始图像');
+subplot(2,2,2);
+imhist(I, 64);
+title('n=64 的直方图');
+subplot(2,2,3);
+imhist(I, 128);
+title('n=128 的直方图');
+subplot(2,2,4);
+imhist(I, 256);
+title('n=256 的直方图');
 
-%5、图像的直方图显示与图像的灰度直方图均衡化
-%(1)图像的直方图显示
-%A．在matlab环境中，程序首先读取图像，然后调用直方图函数，设置相关参数，再输出处理后的图像。
+% 3. 观察不同图像的直方图
+I = imread('Ex1/pollen1.tif');
+J = imread('Ex1/pollen2.tif');
+figure;
+subplot(2,2,1);
+imshow(I);
+subplot(2,2,2);
+imhist(I, 256);
+subplot(2,2,3);
+imshow(J);
+subplot(2,2,4);
+imhist(J, 256);
 
-I=imread('cameraman1.tif');              %读取图像
-subplot(1,2,1),imshow(I)                %输出图像
-title('原始图像')                           %在原始图像中加标题
-subplot(1,2,2),imhist(I)                  %输出原图直方图
-title('原始图像直方图')                     %在原图直方图上加标题
+% 4. 旋转图像前后的直方图对比
+I = imread('Ex1/boat.bmp');
+J = imrotate(I, 180);
+figure;
+subplot(2,2,1);
+imshow(I);
+subplot(2,2,2);
+imhist(I, 256);
+subplot(2,2,3);
+imshow(J);
+subplot(2,2,4);
+imhist(J, 256);
 
-%B．读入图像，在一个窗口中显示灰度级n=64，128和256的图像直方图。
-
-I=imread('rice1.tif');
-subplot(2,2,1);imshow(I);title('原始图像')
-subplot(2,2,2);imhist(I,64);title('n=64的直方图')
-subplot(2,2,3);imhist(I,128);title('n=128的直方图')
-subplot(2,2,4);imhist(I,256);title('n=256的直方图')
-
-
-%C.读入图像'pollen1.tif '和图像'pollen2.tif '，观察其图像和对应的灰度直方图显示。
-
-clear all;
-clc;
-I=imread('pollen1.tif');
-J= imread('pollen2.tif');
-subplot(2,2,1);imshow(I);
-subplot(2,2,2);imhist(I,256);
-subplot(2,2,3);imshow (J);
-subplot(2,2,4);imhist(J,256);
-
-%D. 读入图像'boat.bmp ',将其旋转180度，观察原图和旋转180度后图像的灰度直方图。
-
-clear all;
-clc;
-I=imread('boat.bmp');
-J= imrotate(I,180);
-subplot(2,2,1);imshow(I);
-subplot(2,2,2);imhist(I,256);
-subplot(2,2,3);imshow (J);
-subplot(2,2,4);imhist(J,256);
-
-%(2) 图像的灰度直方图均衡化
-% 在matlab环境中，程序首先读取图像，然后调用灰度直方图均衡化函数，设置相关参数，再输出处理后的图像;观察原图像和直方图均衡化后图像的显示效果，并显示原图像和直方图均衡化后图像的直方图。
-clear all;
-clc;
-I=imread('pollen1.tif');              %读取图像
-subplot(2,2,1),imshow(I)            %输出图像
-title('原始图像')                   %在原始图像中加标题
-subplot(2,2,3),imhist(I,256)                  %输出原图直方图
-title('原始图像直方图')                     %在原图直方图上加标题
-J=histeq(I,256);                    %直方图均衡化，灰度级为256
-subplot(2,2,2),imshow(J)                %输出均衡化后图像
-title('均衡化后图像')                  %在均衡化后图像中加标题
-subplot(2,2,4),imhist(J,256)             %输出均衡化后直方图
-title('均衡化后图像直方图')             %在均衡化后直方图上加标题
+% (B) 图像的灰度直方图均衡化
+I = imread('Ex1/pollen1.tif');
+figure;
+subplot(2,2,1);
+imshow(I);
+title('原始图像');
+subplot(2,2,3);
+imhist(I, 256);
+title('原始图像直方图');
+J = histeq(I, 256); % 直方图均衡化，灰度级为 256
+subplot(2,2,2);
+imshow(J);
+title('均衡化后图像');
+subplot(2,2,4);
+imhist(J, 256);
+title('均衡化后图像直方图');
